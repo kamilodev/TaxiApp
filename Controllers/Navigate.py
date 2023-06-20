ec5a7007dd220d97b7c6073bf112c46db8db211f
ProKav <kamilo_vanegas@hotmail.com>
2023-06-20 16:58:05 +0200

    refactor: Extract logic from main, and separate it in Navigate file from Controllers

---
+++ Controllers/Navigate.py
@@ -0,0 +1,41 @@
+from Controllers.Control_Travel import Control_Travel
+from Views.PrintValues import PrintValues
+from pynput import keyboard
+
+
+class Navigate:
+    def __init__(self):
+        self.control_keys = Control_Travel()
+        self.counter = 0
+        self.printer = PrintValues()
+
+    def on_press(self, key):
+        if key == keyboard.Key.space:
+            if self.counter == 1:
+                self.control_keys.is_move = not self.control_keys.is_move
+                self.control_keys.toggle_fee()
+
+        if key == keyboard.Key.enter:
+            if self.counter == 2:
+                return False
+            self.initial()
+
+    def on_release(self, key):
+        if key == keyboard.Key.esc:
+            return False
+
+    def initial(self):
+        if not self.control_keys.is_new_travel and self.counter % 2 == 0:
+            self.control_keys.new_travel_fee()
+            self.counter += 1
+        elif self.counter % 2 != 0:
+            self.control_keys.new_travel_fee()
+            self.printer.show_info()
+            self.counter += 1
+            input("Tienes que pulsar enter para continuar...")
+            print(self.counter)
+
+    def run(self):
+        listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        listener.start()
+        listener.join()
