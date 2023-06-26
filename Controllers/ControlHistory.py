dc004124746d5fb5907e674fc42e675e1531561f
Camilo Vanegas <kamilo_vanegas@hotmail.com>
2023-06-26 10:18:17 +0200

    feat: Add config logger and logs in ControlHistory and ControlTravel

--- Controllers/ControlHistory.py
+++ Controllers/ControlHistory.py
@@ -1,6 +1,11 @@
 import os
 import json
 import time
+import logging
+from Controllers.AuxFunctions import clear_screen
+from config_logger import setup_logger
+
+logger = setup_logger()
 
 
 def control_history(action: str):
@@ -11,21 +16,25 @@ def control_history(action: str):
     :param action: a string that represents the action to be performed on the control history. It can be
     either "delete" or "show"
     """
+    clear_screen()
+    logger.info(f"Executing control_history with action: {action}")
+
     if not os.path.isfile("history.json"):
-        os.system("cls" if os.name == "nt" else "clear")
-        print("No existe historico")
+        clear_screen()
+        logger.info("No existe historico")
         time.sleep(2)
-        os.system("cls" if os.name == "nt" else "clear")
+        clear_screen()
     else:
         if action == "delete":
             confirmation = input(
                 "¿Está seguro que desea eliminar el archivo de historial? (s/n): "
             )
+            logger.info(f"User confirmation for file deletion: {confirmation}")
             if confirmation.lower() == "s":
                 os.remove("history.json")
-                print("Archivo eliminado")
+                logger.info("Archivo eliminado")
             else:
-                print("Eliminacion abortada")
+                logger.info("Eliminacion abortada")
         elif action == "show":
             show()
         input("Presione Enter para continuar")
@@ -37,14 +46,17 @@ def show():
     file_path = os.path.join(current_directory, "history.json")
     with open(file_path, "r") as file:
         data = json.load(file)
+
+    logger.info("Displaying trip history")
     for index, record in enumerate(data, 1):
-        print("_" * 30)
+        logger.info(f"Trip number: {index}")
         stop_time = record["total_stopped_time"]
         move_time = record["total_movement_time"]
         total_time = record["total_time"]
         bill_stop = record["total_bill_stop"]
         bill_move = record["total_bill_move"]
         bill_total = record["total_bill_total"]
+        print("_" * 30)
         print(
             f"\n🏁 Viaje numero {index} 🏁\n\n"
             f"Tiempo total             : {total_time}\n"
--- Controllers/ControlTravel.py
+++ Controllers/ControlTravel.py
@@ -1,5 +1,6 @@
 from Controllers.Fees import Fees
-
+from config_logger import setup_logger
+logger = setup_logger()
 
 class ControlTravel:
     def __init__(self):
@@ -11,15 +12,19 @@ class ControlTravel:
     def toggle_fee(self):
         if self.is_move:
             self.send_fee.fee_stoped()
+            logger.info("Vehículo detenido")
         else:
             self.send_fee.fee_movement()
+            logger.info("Vehiculo en marcha")
 
     def new_travel_fee(self):
         if not self.is_new_travel:
             self.send_fee.init_end_move()
             self.is_new_travel = True
+            logger.info("Viaje iniciado")
         else:
             data = self.send_fee.end_travel()
             json_data = data.data_to_json()
             data.save_history_to_mongo(json_data)
+            logger.info("Viaje finalizado")
             return False
